285fcb63b991eab1963079575ac78a17cfe1532a
Frank Diaz <email-diazfrankp@gmail.com>
2018-11-05 19:54:47 -0500

    Done with temperature by city only

--- public/main.js
+++ public/main.js
@@ -1,5 +1,51 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+
+const citySearch = () => {
+  let inputArea = document.querySelector('input')
+  let citySelection = inputArea.value
+  let urlCityData = `https://api.openweathermap.org/data/2.5/weather?q=${citySelection}&appid=b0c76d1569358d0297f8ab56efafdb49&units=imperial`
+fetch(urlCityData).then(response => 
+  response.json()).then(weatherData => {
+    console.log(weatherData)
+    let theUL =document.querySelector('ul')
+  let newLI = document.createElement('li')
+  newLI.textContent = `Temperature: ${weatherData.main.temp}°F`
+  theUL.appendChild(newLI)
+})
 }
 
+
+
+// const addTempToUL = temperature => {
+  
+
+
+const main = () => {
+  let searchButton = document.querySelector('.search')
+  searchButton.addEventListener('click', citySearch)
+}
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', main)
+
+
+
+
+
+
+
+
+//------------------------------------------
+//      Writing Out My Algorithm 
+// Find the spot in the user interface where I want to put the temp
+//
+// Create a new element to put the temperature
+// Set the elements text to whatever is inside weather.main.temp
+// Make that element appear on the screen
+
+//my id from OpenWeatherMap:
+//  b0c76d1569358d0297f8ab56efafdb49
