3290e2ad6e8e2510dafa45a60c7d5d1411724a46
Frank Diaz <email-diazfrankp@gmail.com>
2018-11-06 23:33:09 -0500

    Explorer mode complete

--- public/main.js
+++ public/main.js
@@ -19,15 +19,28 @@ class WeatherAPI {
         dom.substitute(temp, humidity, windSpeed)
       })
   }
+  getWeatherForZip(zip) {
+    let zipURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=b0c76d1569358d0297f8ab56efafdb49&units=imperial`
+    fetch(zipURL)
+      .then(response => {
+        return response.json()
+      })
+      .then(weatherData => {
+        let temp = weatherData.main.temp
+        let humidity = weatherData.main.humidity
+        let windSpeed = weatherData.wind.speed
+        let dom = new DocumentObjectModelOne()
+        dom.substitute(temp, humidity, windSpeed)
+      })
+  }
 }
-
+// How can I display textContent of string with of temp, humidity, and windSpeed?
 class DocumentObjectModelOne {
   substitute(temp, humidity, windSpeed) {
     this.appendLI(temp)
     this.appendLI(humidity)
     this.appendLI(windSpeed)
   }
-
   appendLI(value) {
     let theUL = document.querySelector('ul')
     let newLI = document.createElement('li')
@@ -38,23 +51,29 @@ class DocumentObjectModelOne {
     let theInput = document.querySelector('.city-input').value
     return theInput
   }
+  zip() {
+    let theInput = document.querySelector('.zip-input').value
+    return theInput
+  }
 }
 
 userCitySearch = () => {
   let dom = new DocumentObjectModelOne()
   let city = dom.city()
-
   let weatherAPI = new WeatherAPI()
   weatherAPI.getWeatherForCity(city)
 }
 
+userZipSearch = () => {
+  let dom = new DocumentObjectModelOne()
+  let zip = dom.zip()
+  let weatherAPI = new WeatherAPI()
+  weatherAPI.getWeatherForCity(zip)
+}
 const main = () => {
   document.querySelector('.search').addEventListener('click', userCitySearch)
+  document.querySelector('.searchZip').addEventListener('click', userZipSearch)
 }
-//   let zipSearchButton = document.querySelector('.searchZip')
-//   zipSearchButton.addEventListener('click', zipSearch)
-//
-
 document.addEventListener('DOMContentLoaded', main)
 
 /*const citySearch = () => {
